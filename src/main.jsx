fbeb6a6b4d7107640f2e04ab5770ec84f5d3cad6
LauraRosas10 <lalavanesarosas@gmail.com>
2025-04-10 12:00:56 -0500

    creacion nosotros somos

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRouter from './routes/AppRouter.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppRouter/>
